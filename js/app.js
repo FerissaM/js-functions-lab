d3fce39ef5c941e1779621df9f4b195341d71167
Ferissa Mabalot <mabalot.ferissa@gmail.com>
2023-09-27 11:28:01 -0700

    J Functions Lab

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*
+Exercise 1:
+  - Define an empty array named foods
+*/
+
+// Exercise 1 has been completed for you...
+
+const foods = [];
+
+console.log('Exercise 1 Result:\n', foods);
+
+/*
+Exercise 2:
+  - Add the strings 'pizza' & 'cheeseburger' to the foods array such that 'pizza' comes before 'cheeseburger'.
+*/
+
+// Complete Exercise 2 below...
+
+foods.push('pizza', 'cheeseburger');
+
+console.log('Exercise 2 Result:\n',  foods);
+
+/*
+Exercise 3:
+  - Add the string 'taco' to the foods array so that 'taco' is the first food in the array.
+*/
+
+// Complete Exercise 3 below...
+
+foods.unshift('taco');
+
+console.log('Exercise 3 Result:\n', foods);
+
